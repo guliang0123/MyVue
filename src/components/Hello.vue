08437aa038af6ec7e1c5b744480c9ee8add5c206
gl <898148191@qq.com>
2018-01-22 17:56:55 +0800

    搭架脚手架

---
+++ src/components/Hello.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>  <!-- 这里是展示数据中的  -->
+        <h2>Essential 呵呵呵</h2>
+        <ul>
+            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+            <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+            <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+            <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+            <br>
+            <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+        </ul>
+        <h2>Ecosystem 哈哈哈</h2>
+        <ul>
+            <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+            <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+            <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'hello',   /* 这个name暂时不知道用啥用，根据官方文档说的是方便排错的 */
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'   /* 这里是数据，一定记住数据一定要放data中然后用return返回 */
+        }
+    }
+}
+</script>
+        <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>   /*  scoped的意思是这里的样式只对当前页面有效不会影响其他页面，还有可以设置lang="scss"就是支持css预编译，也就是支持sass或者less  */
+h1, h2 {
+    font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
+
+        作者：datura_lj
+        链接：https://www.jianshu.com/p/2b661d01eaf8
+        來源：简书
+        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
